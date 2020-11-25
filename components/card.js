cd296ecebd3a1c9f259f341d6661eefa63e90380
Robbie Baker <robbiethebakerman@googlemail.com>
2020-11-25 20:56:26 +0000

    Add link option to card.js

--- components/card.js
+++ components/card.js
@@ -1,10 +1,24 @@
 import styles from "../styles/Card.module.css";
 
-export default function Card({ title, image }) {
-    return (
-        <div className={styles.container}>
-            <img src={image.src} alt={image.alt}></img>
-            <h2>{title}</h2>
-        </div>
-    );
+export default function Card({ title, image, link }) {
+    if (link) {
+        return (
+            <a
+                className={styles.container}
+                href={link.href}
+                target={link.target || "_blank"}
+                rel="noopener noreferrer"
+            >
+                <img src={image.src} alt={image.alt}></img>
+                <h2>{title}</h2>
+            </a>
+        );
+    } else {
+        return (
+            <div className={styles.container}>
+                <img src={image.src} alt={image.alt}></img>
+                <h2>{title}</h2>
+            </div>
+        );
+    }
 }
