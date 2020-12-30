51764a061c7db91707e2fe4cf277284b4590d737
Robbie Baker <robbiethebakerman@googlemail.com>
2020-12-30 17:15:22 +0000

    Collection page tidy styling

--- pages/collection.js
+++ pages/collection.js
@@ -1,11 +1,25 @@
-import common from "../styles/Common.module.css";
 import Layout from "../components/layout";
 import CardGrid from "../components/cardGrid";
+import styles from "../styles/Collection.module.css";
 
 export default function Collection() {
     const collections = [
         {
             title: "Textiles",
+            image: {
+                src: "/CB.png",
+                alt: "Charlotte Baker Textiles logo",
+            },
+        },
+        {
+            title: "Paper",
+            image: {
+                src: "/CB.png",
+                alt: "Charlotte Baker Textiles logo",
+            },
+        },
+        {
+            title: "Accessories",
             image: {
                 src: "/CB.png",
                 alt: "Charlotte Baker Textiles logo",
@@ -17,16 +31,18 @@ export default function Collection() {
         <Layout>
             <h1>Collection</h1>
             <CardGrid cardData={collections} />
-            <p>
-                A someone personally interested, I have a collection of objects
-                related to fashion history and sewing paraphernalia.
-            </p>
-            <p>
-                Eergqerh eh eqh qhlekwq hek heqwk wehq kweq hewqh. E rqehe heq
-                ewh kl ewqh ek hqejwhjqwe hweh qew h kew khq ewh wqh, eh keqh
-                khe ewh. T erhqeqrher heqrk rqeqrek rebk kre njeqrk jh hr eq
-                breq bb.
-            </p>
+            <div className={styles.textBody}>
+                <p>
+                    A someone personally interested, I have a collection of
+                    objects related to fashion history and sewing paraphernalia.
+                </p>
+                <p>
+                    Eergqerh eh eqh qhlekwq hek heqwk wehq kweq hewqh. E rqehe
+                    heq ewh kl ewqh ek hqejwhjqwe hweh qew h kew khq ewh wqh, eh
+                    keqh khe ewh. T erhqeqrher heqrk rqeqrek rebk kre njeqrk jh
+                    hr eq breq bb.
+                </p>
+            </div>
         </Layout>
     );
 }
