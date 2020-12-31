77b633a4fe31f2aef1a2ffc8537a8c45822b337a
Robbie Baker <robbiethebakerman@googlemail.com>
2020-12-31 18:25:36 +0000

    get IG posts media page (hard-coded token)

--- pages/media.js
+++ pages/media.js
@@ -2,7 +2,7 @@ import common from "../styles/Common.module.css";
 import Layout from "../components/layout";
 import CardGrid from "../components/cardGrid";
 
-export default function Media() {
+export default function Media({ posts }) {
     const media = [
         {
             title: "Blog Post 1",
@@ -20,10 +20,26 @@ export default function Media() {
         },
     ];
 
+    console.log("IG posts: ", posts);
+
     return (
         <Layout>
             <h1>Media</h1>
             <CardGrid cardData={media} />
         </Layout>
     );
+}
+
+export async function getStaticProps() {
+    const res = await fetch(
+        "https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url,username,timestamp&access_token=IGQVJXdzlYSnp4aEZAvVVNleEtTVU1aZA0xxcGFzSEp1YWc4SG54LTVyNF9BQW9wc09xa1J4U3Bsb3gwYXc4blo5eW9kYlpGLVRhWUlVZATNLLXNQT3I4cEhBeTZAnZA2pUbGtPRHZAaREthOGdBNXIxYm5TeQZDZD"
+    );
+    const posts = await res.json();
+
+    return {
+        props: {
+            posts,
+        },
+        revalidate: 1, // In seconds
+    };
 }
