159b4b40a5ca6f0dd07cd571c5a2cc3fb30b3440
Robbie Baker <robbiethebakerman@googlemail.com>
2020-12-31 20:19:51 +0000

    Display instagram posts

--- pages/media.js
+++ pages/media.js
@@ -1,4 +1,5 @@
-import common from "../styles/Common.module.css";
+import cardGrid from "../styles/CardGrid.module.css";
+import styles from "../styles/Media.module.css";
 import Layout from "../components/layout";
 import CardGrid from "../components/cardGrid";
 
@@ -20,12 +21,41 @@ export default function Media({ posts }) {
         },
     ];
 
-    console.log("IG posts: ", posts);
+    const igLinks = posts.data.slice(0, 3).map((item) => {
+        return (
+            <a
+                className={styles.igLink}
+                href={item.permalink}
+                target="_blank"
+                rel="noopener noreferrer"
+                key={item.id}
+            >
+                <img
+                    src={item.media_url}
+                    alt="Instagram post from Charlotte Baker Textiles"
+                ></img>
+            </a>
+        );
+    });
 
     return (
         <Layout>
             <h1>Media</h1>
             <CardGrid cardData={media} />
+            <h2>
+                instagram{" "}
+                <a
+                    className={styles.link}
+                    href="https://www.instagram.com/charlottebakertextiles/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    @charlottebakertextiles
+                </a>
+            </h2>
+            <div className={cardGrid.container}>
+                <div className={cardGrid.grid3column}>{igLinks}</div>
+            </div>
         </Layout>
     );
 }
