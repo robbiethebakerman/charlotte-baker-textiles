59c687a9aeebef59a86b75ee35ff6f19c70943c7
Robbie Baker <robbiethebakerman@googlemail.com>
2021-01-24 16:11:11 +0000

    Move text Collection page

--- pages/collection.js
+++ pages/collection.js
@@ -17,10 +17,10 @@ export default function Collection() {
     return (
         <Layout>
             <h1>Collection</h1>
-            <CardGrid cardData={collections} imageContainerHeight={"218px"} />
             <div className={styles.textBody}>
                 <p>This page is currently under construction.</p>
             </div>
+            <CardGrid cardData={collections} imageContainerHeight={"218px"} />
         </Layout>
     );
 }
