c078e15c62d3e49b86872b88f3300c641a5d53bf
Robbie Baker <robbiethebakerman@googlemail.com>
2021-01-24 16:31:26 +0000

    Add collection placeholders and new paper image

--- pages/collection.js
+++ pages/collection.js
@@ -6,9 +6,23 @@ import common from "../styles/Common.module.css";
 export default function Collection() {
     const collections = [
         {
+            title: "Textiles",
+            image: {
+                src: "https://via.placeholder.com/264x199",
+                alt: "",
+            },
+        },
+        {
             title: "Paper",
             image: {
                 src: "/Collection_Paper.jpg",
+                alt: "",
+            },
+        },
+        {
+            title: "Accessories",
+            image: {
+                src: "https://via.placeholder.com/264x199",
                 alt: "",
             },
         },
@@ -20,7 +34,7 @@ export default function Collection() {
             <div className={styles.textBody}>
                 <p>This page is currently under construction.</p>
             </div>
-            <CardGrid cardData={collections} imageContainerHeight={"218px"} />
+            <CardGrid cardData={collections} imageContainerHeight={"215px"} />
         </Layout>
     );
 }
