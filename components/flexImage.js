49d6c3cbf1496bf29605e3682688530910e93ae1
Robbie Baker <robbiethebakerman@googlemail.com>
2021-01-30 20:56:24 +0000

    About - smaller image, smaller width

--- components/flexImage.js
+++ components/flexImage.js
@@ -1,10 +1,20 @@
 import styles from "../styles/FlexImage.module.css";
 
-export default function FlexImage({ src, alt, caption, containerHeight }) {
+export default function FlexImage({
+    src,
+    alt,
+    caption,
+    containerHeight,
+    maxwidth,
+}) {
     return (
         <div
             className={styles.imageContainer}
-            style={{ height: containerHeight, minHeight: containerHeight }}
+            style={{
+                height: containerHeight,
+                minHeight: containerHeight,
+                maxWidth: maxwidth,
+            }}
         >
             <div>
                 <img src={src} alt={alt}></img>
--- pages/about.js
+++ pages/about.js
@@ -6,36 +6,45 @@ import FlexImage from "../components/flexImage";
 export default function About() {
     return (
         <Layout>
-            <h1>About Me</h1>
-            <div className={styles.grid}>
-                <FlexImage
-                    src="/CharlotteProfileWorking.jpg"
-                    alt="Charlotte Baker with a mounted dress"
-                    caption="Image blurb"
-                />
-                <div className={common.textBody}>
-                    <p>
-                        Charlotte Baker specialises in the preservation and
-                        conservation of textile objects, having graduated from
-                        the Centre for Textile Conservation, University of
-                        Glasgow with an MPhil in Textile Conservation (2019).
-                        After graduating Charlotte completed a year-long Textile
-                        Conservation Internship at the Bowes Museum supported by
-                        the Institute of Conservation and the Heritage Lottery
-                        Fund.
-                    </p>
-                    <p>
-                        This website, which is currently under construction,
-                        will provide a platform to highlight some of the
-                        conservation services that will be available to
-                        institutions and private individuals.
+            <div className={styles.container}>
+                <div className={styles.content}>
+                    <h1>About Me</h1>
+                    <div className={styles.grid}>
+                        <div className={styles.flexImageContainer}>
+                            <FlexImage
+                                src="/CharlotteProfileWorking.jpg"
+                                alt="Charlotte Baker with a mounted dress"
+                                caption="Image blurb"
+                                maxwidth="500px"
+                            />
+                        </div>
+                        <div className={common.textBody}>
+                            <p>
+                                Charlotte Baker specialises in the preservation
+                                and conservation of textile objects, having
+                                graduated from the Centre for Textile
+                                Conservation, University of Glasgow with an
+                                MPhil in Textile Conservation (2019). After
+                                graduating Charlotte completed a year-long
+                                Textile Conservation Internship at the Bowes
+                                Museum supported by the Institute of
+                                Conservation and the Heritage Lottery Fund.
+                            </p>
+                            <p>
+                                This website, which is currently under
+                                construction, will provide a platform to
+                                highlight some of the conservation services that
+                                will be available to institutions and private
+                                individuals.
+                            </p>
+                        </div>
+                    </div>
+                    <h2>Contact</h2>
+                    <p className={styles.contactText}>
+                        Please feel free to contact me for enquiries.
                     </p>
                 </div>
             </div>
-            <h2>Contact</h2>
-            <p className={styles.contactText}>
-                Please feel free to contact me for enquiries.
-            </p>
         </Layout>
     );
 }
