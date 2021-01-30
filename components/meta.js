b5a94b8ae7d0c9e40850c31d250771432c318150
Robbie Baker <robbiethebakerman@googlemail.com>
2021-01-30 15:25:48 +0000

    Change font and font color to europa dark grey

--- components/meta.js
+++ components/meta.js
@@ -5,6 +5,10 @@ export default function Meta() {
         <Head>
             <title>Charlotte Baker Textiles</title>
             <link rel="icon" href="CB.png" />
+            <link
+                rel="stylesheet"
+                href="https://use.typekit.net/vln6ojf.css"
+            ></link>
         </Head>
     );
 }
