9dc24d09e5a8fbe9bb85c7695c6478e4d9ddaed7
Robbie Baker <robbiethebakerman@googlemail.com>
2021-02-24 10:59:39 +0000

    Removed unused code slide.js

--- components/slide.js
+++ components/slide.js
@@ -1,17 +1,8 @@
 import styles from "../styles/Slide.module.css";
 
 export default function Slide({ imageData }) {
-    const images = imageData.map((image, index, array) => {
-        // const marginRight = index < array.length - 1 ? "1rem" : undefined;
-
-        return (
-            <img
-                key={index}
-                src={image.src}
-                alt={image.alt}
-                // style={{ marginRight: marginRight }}
-            ></img>
-        );
+    const images = imageData.map((image, index) => {
+        return <img key={index} src={image.src} alt={image.alt}></img>;
     });
 
     return <div className={styles.slide}>{images}</div>;
